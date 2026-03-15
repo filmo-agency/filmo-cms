cf7719765ee399f2b75a8d6672792f2f432e005d
David Sanchez <dsebastiansr24@gmail.com>
2026-03-15 11:43:35 -0500

    [ADD] CORS

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -7,10 +7,10 @@ module.exports = [
       origin: [
         'http://192.168.100.125:3000',
         'http://localhost:3000',
-        'https://www.filmostudio.com/',
+        'http://localhost:4321',
+        'https://www.filmostudio.com',
         'https://filmostudio.com',
-        'https://filmo-web.vercel.app'
-
+        'https://filmo-web.vercel.app',
       ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization'],
